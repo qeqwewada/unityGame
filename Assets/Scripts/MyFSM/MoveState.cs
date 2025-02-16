d52ea269f0f7fc1b2135a41b97ee80b21d8e44f6
qwq <17390811025@163.com>
2025-02-16 17:39:12 +0800

    2333

--- Assets/Scripts/MyFSM/MoveState.cs
+++ Assets/Scripts/MyFSM/MoveState.cs
@@ -38,12 +38,12 @@ namespace MyFSM
             {
                 Vector3 input;
                 
-                    // 处理旋转的问题
+                    
                     input = new Vector3(h, 0, v);
               
-                // 获取相机的旋转值 y
+               
                 float y = Camera.main.transform.rotation.eulerAngles.y;
-                // 让四元数和向量相乘：表示让这个向量按照这个四元数所表达的角度进行旋转后得到新的向量
+                
                 Vector3 targetDir = Quaternion.Euler(0, y, 0) * input;
                 kalie.transform.rotation = Quaternion.Slerp(kalie.transform.rotation, Quaternion.LookRotation(targetDir), Time.deltaTime * kalie.playerParameter.rotateSpeed);
                 kalie.playerParameter.characterController.Move(targetDir.normalized * (kalie.playerParameter.walkSpeed * Time.deltaTime));
